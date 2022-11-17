9163c82f063b58987f6926367c5eae5895e3cfad
DanyaDem <114875011+DanyaDem@users.noreply.github.com>
2022-11-17 05:49:44 +0300

    Add files via upload

---
+++ вс/main.js
@@ -0,0 +1,282 @@
+// function a() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.add('www')
+// }
+// a()
+// function a() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.remove('www')
+// }
+// a()
+// function a() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.contains('www')
+// }
+// a()
+// function a() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.toggle('www')
+// }
+// a()
+// function a() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.length
+// }
+// a()
+// function a() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.length
+//     for (let el of elem.classList) {
+//         alert(el)
+//     }
+// }
+// a()
+
+// function a() {
+//     let elem = document.getElementById('elem');
+//     elem.style.cssText = 'color: red; font-size: 30px; border: 1px solid red';
+// }
+// a()
+
+// function a(){
+//     let elem = document.getElementById('elem');
+//     console.log(elem.tagName);
+// }
+
+// function a() {
+//     let elem = document.getElementById('elem');
+//     elem.innerHTML = elem.innerHTML.toLowerCase();
+// }
+
+// function a() {
+//     let elements = document.querySelectorAll ('.www');
+//         for (elem of elements){
+//             elem.innerHTML = elem.innerHTML + elem.tagName.toUpperCase();
+//         }
+//     }
+
+
+
+// const a = document.querySelector('.a');
+// a.insertAdjacentHTML('beforeBegin', '<span>!!!</span>');
+
+// const b = document.querySelector('.b');
+// b.insertAdjacentHTML('afterEnd', '<span>!!!</span>');
+
+// const c= document.querySelector('.c');
+// c.insertAdjacentHTML('afterBegin', '<span>!!!</span>');
+
+// const d= document.querySelector('.d');
+// d.insertAdjacentHTML('beforeEnd', '<span>!!!</span>');
+
+
+
+
+// const getBtn = document.querySelector('.btn');
+// const getP = document.querySelector('.elem');
+// const getSpan = document.querySelector('.elem2');
+
+// getBtn.addEventListener('click', func);
+
+// function func() {
+//     getP.removeChild(getSpan);
+// };
+
+
+
+// const getBtn28 = document.querySelector('.btn28');
+// const getOl28 = document.querySelector('.task28');
+// const getLast = getOl28.lastElementChild;
+
+// getOl28.removeChild(getLast);                  
+
+
+// const getBtn = document.querySelector('.btn');
+
+// getBtn.addEventListener('click', func);
+
+// function func() {
+//     let getParent = getBtn.parentElement;
+//     getParent.removeChild(getBtn);
+// };
+
+
+
+// const getLiList = document.querySelectorAll('.t1');
+
+//         for (let i = 0; i < getLiList.length; i += 1) {
+//             getLiList[i].addEventListener('click', func);
+//         }
+
+//         function func() {
+//             let getParent = this.parentElement;
+//             getParent.removeChild(this);
+//         };
+
+
+
+// const getInput31 = document.querySelector('#id31');
+// const getBtn31 = document.querySelector('.btn31');
+
+//         getBtn31.addEventListener('click', func31);
+
+//         function func31() {
+//             const clone31 = getInput31.cloneNode(true);  
+//             console.log(clone31);
+//         };
+
+
+
+
+
+
+// const getP = document.querySelector('.elem');
+
+// getP.firstElementChild.style.color = 'red';
+
+
+// const getP = document.querySelector('.elem');
+
+// getP.lastElementChild.style.color = 'red';
+
+
+
+
+
+// const getP = document.querySelector('.elem');
+//         const elems = getP.children;
+
+//         for (let i = 0; i < elems.length; i += 1) {
+//             elems[i].innerHTML = `${elems[i].innerHTML}!`;
+//         }
+
+
+
+// let getSpan = document.querySelector('.element');
+// let element = getSpan.previousElementSibling;
+// element.innerHTML = `${element.innerHTML}!`;
+
+
+
+
+// let getSpan = document.querySelector('.element');
+// let element = getSpan.nextElementSibling;
+// element.innerHTML = `${element.innerHTML}!`;
+
+
+
+// let getSpan = document.querySelector('.element');
+// let element = getSpan.nextElementSibling;
+// let nextelem = element.nextElementSibling;
+// nextelem.innerHTML = `${nextelem.innerHTML}?`;      
+
+
+
+// let elem = document.getElementById('elem');
+// let button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.clientTop);
+// }
+
+// let elem = document.getElementById('elem');
+// let button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.clientLeft);
+// }
+
+
+
+
+
+// let elem = document.getElementById('elem');
+// let button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.offsetWidth);
+// }
+
+
+// let elem = document.getElementById('elem');
+// let button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.offsetHeight);
+// }
+
+
+// let elem = document.getElementById('elem');
+// let button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.clientWidth);
+// }
+
+// let elem = document.getElementById('elem');
+// let button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.clientHeight);
+// }
+
+// let elem = document.getElementById('elem');
+// let button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.scrollTop);
+// }
+
+// let elem = document.getElementById('elem');
+// let button = document.getElementById('button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	alert(elem.scrollLeft);
+// }
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
